c32d0cf5c720204a8ffe6137f10cb2c87c4d592b
Bhuang <bryan.huang@excella.com>
2015-04-19 22:51:26 -0700

    Initial commit.  Ridded myself of a storyboard

--- FitnessApp/AppDelegate.swift
+++ FitnessApp/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        var mainViewController = MainWindow(nibName: "MainWindow", bundle: nil)
+        
+        window?.rootViewController = mainViewController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
